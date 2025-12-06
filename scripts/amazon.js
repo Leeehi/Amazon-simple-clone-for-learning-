5de4862a26e0d6ba523a5cb70653576a7b1b64b8
Lehi Galabo <galabslehi75@gmail.com>
2025-12-07 02:38:02 +0800

    added more products

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,33 +1,3 @@
-const product = [
-  {
-  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating: {
-    star: 4.5,
-    count: 87,
-  },
-  price: 1090
-  },
-  {
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-      star: 4.0,
-      count: 127
-    },
-    price: 2095 
-  },
-  {
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-      star: 4.5,
-      count: 56
-    },
-    price: 799
-  }
-];
-
 let productHTML = '';
 
 product.forEach( (products) => {
@@ -51,7 +21,7 @@ product.forEach( (products) => {
           </div>
 
           <div class="product-price">
-            ${(products.price / 100).toFixed(2)}
+            $${(products.price / 100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
