8f9fcd910f44b7a243b089babc2cf2aa57d9dd26
Lehi Galabo <galabslehi75@gmail.com>
2026-01-16 00:45:41 +0800

    added a practice for backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,6 +3,7 @@ import { renderPaymentSummary } from './checkout/paymentSummary.js';
 import { renderCheckout } from './checkout/renderCheckoutHeader.js'
 // import '../data/cart-class.js';
 // import { cars } from '../data/car.js';
+import '../data/backend-practice.js'
 
 
 renderOrderSummary();
